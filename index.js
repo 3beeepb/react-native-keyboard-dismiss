d97e08876b344f23f44a9fa4c806f4a941e394d9
Pavel Vladimiroff <pavel.vladimiroff@gmail.com>
2021-01-24 09:47:41 +0300

    bugs fix

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { TouchableWithoutFeedback, Keyboard, View } from 'react-native'
 
-export default KeyboardDismiss = (children) => {
-	return (
+const KeyboardDismissHOC = (Comp) => {
+	return ({children, ...props}) => (
 		<TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
 			<View style={{flex:1}}>
-				{children}
+				<Comp {...props}>
+					{children}
+				</Comp>
 			</View>
 		</TouchableWithoutFeedback>
 	)
 }
+export default KeyboardDismiss = KeyboardDismissHOC(View)
